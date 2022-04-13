547d2d6219751cd455f8a6e957d45fc6d857dff2
carlaboscolo <100995659+carlaboscolo@users.noreply.github.com>
2022-04-13 16:53:11 +0200

    FragmentCalendar

---
+++ ToDoGruppo/app/src/main/java/com/example/todogruppo/CalendarFragment.kt
@@ -0,0 +1,28 @@
+package com.example.todogruppo
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [CalendarFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+
+class CalendarFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_calendar, container, false)
+    }
+
+
+}
