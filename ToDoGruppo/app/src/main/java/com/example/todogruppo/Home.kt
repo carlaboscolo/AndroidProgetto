ff291b3f0127dfb9f1ff41d5e1c31fbb4e355cd3
carlaboscolo <100995659+carlaboscolo@users.noreply.github.com>
2022-04-18 15:56:58 +0200

    Funzionamento login (con credenziali pre-impostate)

--- ToDoGruppo/app/src/main/java/com/example/todogruppo/Home.kt
+++ ToDoGruppo/app/src/main/java/com/example/todogruppo/Home.kt
@@ -1,32 +1,33 @@
 package com.example.todogruppo
+
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationBarView
 
-class Home : AppCompatActivity()  {
+class HomePage : MainActivity() {
+    val name = "HOME"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home)
 
-    val transaction = supportFragmentManager.beginTransaction()
-        .add(R.id.fragmentContainer, HomeFragment.newInstance("", ""))
-    transaction.commit()
 
-    NavigationBarView.OnItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.home -> {
-                // Respond to navigation item 1 click
-                true
-            }
-            else -> {
+        val transaction = supportFragmentManager.beginTransaction()
+            .add(R.id.fragmentContainer, HomeFragment.newInstance("", ""))
+        transaction.commit()
+
+        NavigationBarView.OnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.home -> {
+                    // Respond to navigation item 1 click
+                    true
+                } else -> {
                 false
             }
+            }
         }
-
     }
+
 }
 
 
-}
--- ToDoGruppo/app/src/main/java/com/example/todogruppo/MainActivity.kt
+++ ToDoGruppo/app/src/main/java/com/example/todogruppo/MainActivity.kt
@@ -1,19 +1,52 @@
 package com.example.todogruppo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 
 
+open class MainActivity : AppCompatActivity() {
 
-class MainActivity : AppCompatActivity() {
 
+    private lateinit var username : EditText
+    private lateinit var password : EditText
+    private lateinit var login : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val username = findViewById<EditText>(R.id.username)
+        val password = findViewById<EditText>(R.id.password)
+        val button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener {
+            if (username.text.toString().isEmpty() && password.text.toString().isEmpty()){
+                Log.d("log", "credential empty ")
+            }else {
+                launchSecondActivity()
+            }
+        }
     }
 
+
+private fun launchSecondActivity() {
+    val username = findViewById<EditText>(R.id.username)
+    val password = findViewById<EditText>(R.id.password)
+
+    val us = "carlaboscolo@gmail.com"
+    val psw = "123"
+
+    if ((username.text.toString() == us) && (password.text.toString() == psw)) {
+        val intent = Intent(this, HomePage::class.java)
+        startActivity(intent)
+    } else {
+        Log.d("log", "error credential")
+    }
 }
 
 
+}
