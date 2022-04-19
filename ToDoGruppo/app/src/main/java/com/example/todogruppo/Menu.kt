ba50b50e5ba12673caeaa35ab6484778fad10e03
carlaboscolo <100995659+carlaboscolo@users.noreply.github.com>
2022-04-19 17:49:03 +0200

    Fragment
    Non funzionano

--- ToDoGruppo/app/src/main/java/com/example/todogruppo/Menu.kt
+++ ToDoGruppo/app/src/main/java/com/example/todogruppo/Menu.kt
@@ -1,7 +1,9 @@
 package com.example.todogruppo
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import com.example.fragment.CalendarFragment
 import com.example.fragment.HomeFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -11,19 +13,17 @@ class HomePage : MainActivity() {
     val name = "HOME"
 
 
-
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home)
 
         val homeFragment = HomeFragment()
-        val calendarFragment  = CalendarFragment()
+        val calendarFragment = CalendarFragment()
         setCurrentFragment(homeFragment)
 
+
         NavigationBarView.OnItemSelectedListener { item ->
-            when(item.itemId){
+            when (item.itemId) {
                 R.id.home -> {
                     setCurrentFragment(homeFragment)
                     true
@@ -33,64 +33,12 @@ class HomePage : MainActivity() {
                     true
                 }
                 else -> {
-
-                    supportFragmentManager.beginTransaction()
-                    .add(R.id.fragmentContainer, HomeFragment.newInstance("", ""))
-                    .commit()
-
-                false
+                    false
                 }
             }
-
         }
-
-             /* when (item.itemId) {
-                   R.id.home -> {
-                    // Respond to navigation item 1 click
-
-                       supportFragmentManager.beginTransaction()
-                           .add(R.id.fragmentContainer, HomeFragment.newInstance("", ""))
-
-                           .commit()
-
-                       true
-                   }
-                  R.id.checklist -> {
-                      // Respond to navigation item 1 click
-                      true
-                  }
-                  R.id.note-> {
-                      // Respond to navigation item 1 click
-                      true
-                  }
-                 R.id.diario -> {
-                      // Respond to navigation item 1 click
-                      true
-                  }
-                     R.id.calendario-> {
-                        // Respond to navigation item 1 click
-
-                         supportFragmentManager.beginTransaction()
-                             .add(R.id.fragmentContainer, CalendarFragment.newInstance("", ""))
-                             .addToBackStack(null)
-                             .commit()
-
-                        true
-                    }
-                 /* R.id.focus -> {
-                      // Respond to navigation item 1 click
-                      true
-                  }
-                  */
-
-                  else -> {
-                false
-              }
-            }
-        }
-*/
-
     }
+
 
     private fun setCurrentFragment(fragment: Fragment ) =
         supportFragmentManager.beginTransaction().apply {
