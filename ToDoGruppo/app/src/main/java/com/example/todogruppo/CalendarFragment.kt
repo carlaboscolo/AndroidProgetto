2ef6a6a2b39b2e0c9c597ea196d36a8170f5346b
Carla <100995659+carlaboscolo@users.noreply.github.com>
2022-06-09 18:18:46 +0200

    Aggiunto schermata calendario

--- ToDoGruppo/app/src/main/java/com/example/todogruppo/CalendarFragment.kt
+++ ToDoGruppo/app/src/main/java/com/example/todogruppo/CalendarFragment.kt
@@ -1,18 +1,32 @@
 package com.example.fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CalendarView
+import android.widget.CalendarView.OnDateChangeListener
+import android.widget.TextView
 import androidx.fragment.app.Fragment
-import com.example.todogruppo.R
+import androidx.fragment.app.viewModels
+import com.example.todogruppo.checklist.task.viewModel.ViewModel
 import com.example.todogruppo.databinding.FragmentCalendarBinding
+import java.lang.String.format
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class CalendarFragment : Fragment() {
+
+    //view model
+    val viewModel: ViewModel by viewModels()
 
     private lateinit var binding: FragmentCalendarBinding
 
     //variabili
+    private lateinit var day : CalendarView
+    private lateinit var selectedDate : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,8 +44,32 @@ class CalendarFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+
+
+        selectedDate = binding.outputDay
+        day = binding.calendarView
+
+        //data di default
+        val defaultDay = viewModel.calendar()
+        selectedDate.setText(defaultDay)
+
+        day.setOnDateChangeListener(OnDateChangeListener { view, year, month, dayOfMonth ->
+            val day = dayOfMonth.toString()
+            val year = year.toString()
+            val month = month.toString()
+
+            val dayString = if(day.toInt() < 10) "0$day" else "$day"
+            val monthString = if(month.toInt() < 10) "0$month" else "$month"
+
+            val data_string = "$dayString/$monthString/$year"
+
+            //Log.d("data", data_string)
+
+            selectedDate.setText(data_string)
+        })
     }
 
-}
 
+}
 
