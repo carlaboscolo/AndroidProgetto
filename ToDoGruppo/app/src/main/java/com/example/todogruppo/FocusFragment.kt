c8a6bf705f209495b23c8eff53f46265efd20745
Carla <100995659+carlaboscolo@users.noreply.github.com>
2022-07-03 19:22:35 +0200

    Aggiunto focus

--- ToDoGruppo/app/src/main/java/com/example/todogruppo/FocusFragment.kt
+++ ToDoGruppo/app/src/main/java/com/example/todogruppo/FocusFragment.kt
@@ -1,19 +1,37 @@
 package com.example.todogruppo
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.CountDownTimer
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.example.todogruppo.databinding.FragmentFocusBinding
-import com.example.todogruppo.databinding.HomeBinding
+import java.util.*
+
 
 class FocusFragment : Fragment() {
 
     private lateinit var binding: FragmentFocusBinding
 
     //variabili
-
+    private lateinit var mEditTextInput: EditText
+    private lateinit var mTextViewCountDown: TextView
+    private lateinit var mButtonSet: Button
+    private lateinit var mButtonStartPause: Button
+    private lateinit var mButtonReset: Button
+    private lateinit var mCountDownTimer: CountDownTimer
+    private var mTimerRunning = false
+    private var mStartTimeInMillis: Long = 0
+    private var mTimeLeftInMillis: Long = 0
+    private var mEndTime: Long = 0
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -31,6 +49,145 @@ class FocusFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+
+        mEditTextInput = binding.editTextInput
+        mTextViewCountDown = binding.textViewCountdown
+        mButtonSet = binding.buttonSet
+        mButtonStartPause = binding.buttonStartPause
+        mButtonReset = binding.buttonReset
+
+        mButtonSet.setOnClickListener(View.OnClickListener {
+            val input = mEditTextInput.getText().toString()
+            if (input.length == 0) {
+                Log.d("error", "campo vuoto")
+            }
+            val millisInput = input.toLong() * 60000
+            if (millisInput == 0L) {
+                Log.d("error", "inserisci un campo positivo")
+            }
+            setTime(millisInput)
+            mEditTextInput.setText("")
+        })
+        mButtonStartPause.setOnClickListener(View.OnClickListener {
+            if (mTimerRunning) {
+                pauseTimer()
+            } else {
+                startTimer()
+            }
+        })
+        mButtonReset.setOnClickListener(View.OnClickListener { resetTimer() })
+    }
+    private fun setTime(milliseconds: Long) {
+        mStartTimeInMillis = milliseconds
+        resetTimer()
     }
 
+    private fun startTimer() {
+        mEndTime = System.currentTimeMillis() + mTimeLeftInMillis
+        mCountDownTimer = object : CountDownTimer(mTimeLeftInMillis, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                mTimeLeftInMillis = millisUntilFinished
+                updateCountDownText()
+            }
+
+            override fun onFinish() {
+                mTimerRunning = false
+                updateWatchInterface()
+            }
+        }.start()
+        mTimerRunning = true
+        updateWatchInterface()
+    }
+
+    private fun pauseTimer() {
+        mCountDownTimer!!.cancel()
+        mTimerRunning = false
+        updateWatchInterface()
+    }
+
+    private fun resetTimer() {
+        mTimeLeftInMillis = mStartTimeInMillis
+        updateCountDownText()
+        updateWatchInterface()
+    }
+
+    private fun updateCountDownText() {
+        val hours = (mTimeLeftInMillis / 1000).toInt() / 3600
+        val minutes = (mTimeLeftInMillis / 1000 % 3600).toInt() / 60
+        val seconds = (mTimeLeftInMillis / 1000).toInt() % 60
+        val timeLeftFormatted: String
+        timeLeftFormatted = if (hours > 0) {
+            String.format(
+                Locale.getDefault(),
+                "%d:%02d:%02d", hours, minutes, seconds
+            )
+        } else {
+            String.format(
+                Locale.getDefault(),
+                "%02d:%02d", minutes, seconds
+            )
+        }
+        mTextViewCountDown!!.text = timeLeftFormatted
+    }
+
+    private fun updateWatchInterface() {
+        if (mTimerRunning) {
+            mEditTextInput!!.visibility = View.INVISIBLE
+            mButtonSet!!.visibility = View.INVISIBLE
+            mButtonReset!!.visibility = View.INVISIBLE
+            mButtonStartPause!!.text = "Pause"
+        } else {
+            mEditTextInput!!.visibility = View.VISIBLE
+            mButtonSet!!.visibility = View.VISIBLE
+            mButtonStartPause!!.text = "Start"
+            if (mTimeLeftInMillis < 1000) {
+                mButtonStartPause!!.visibility = View.INVISIBLE
+            } else {
+                mButtonStartPause!!.visibility = View.VISIBLE
+            }
+            if (mTimeLeftInMillis < mStartTimeInMillis) {
+                mButtonReset!!.visibility = View.VISIBLE
+            } else {
+                mButtonReset!!.visibility = View.INVISIBLE
+            }
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        val prefs = this.activity!!.getSharedPreferences("pref", Context.MODE_PRIVATE)
+        val editor = prefs.edit()
+        editor.putLong("startTimeInMillis", mStartTimeInMillis)
+        editor.putLong("millisLeft", mTimeLeftInMillis)
+        editor.putBoolean("timerRunning", mTimerRunning)
+        editor.putLong("endTime", mEndTime)
+        editor.apply()
+        if (mCountDownTimer != null) {
+            mCountDownTimer!!.cancel()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val prefs = this.activity!!.getSharedPreferences("pref", Context.MODE_PRIVATE)
+        mStartTimeInMillis = prefs.getLong("startTimeInMillis", 600000)
+        mTimeLeftInMillis = prefs.getLong("millisLeft", mStartTimeInMillis)
+        mTimerRunning = prefs.getBoolean("timerRunning", false)
+        updateCountDownText()
+        updateWatchInterface()
+        if (mTimerRunning) {
+            mEndTime = prefs.getLong("endTime", 0)
+            mTimeLeftInMillis = mEndTime - System.currentTimeMillis()
+            if (mTimeLeftInMillis < 0) {
+                mTimeLeftInMillis = 0
+                mTimerRunning = false
+                updateCountDownText()
+                updateWatchInterface()
+            } else {
+                startTimer()
+            }
+        }
+    }
 }
